869e80e1fe55c212a9baedf1cdd8c39035d9009d
Bruno Miguel <brunomiguel.bcs@gmail.com>
2023-04-18 09:09:46 +0100

    melhor responsividade

--- javascript/scriptPlayground.js
+++ javascript/scriptPlayground.js
@@ -1,6 +1,7 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 
+
 buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
         if(btn.id === "="){
